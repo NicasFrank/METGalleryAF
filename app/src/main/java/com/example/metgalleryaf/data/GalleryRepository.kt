b5f6db4e976435544ce0c131728c10087785f21e
NicasFrank <nicas.frank@gmail.com>
2023-04-13 14:06:24 +0200

    Item Model

--- app/src/main/java/com/example/metgalleryaf/data/GalleryRepository.kt
+++ app/src/main/java/com/example/metgalleryaf/data/GalleryRepository.kt
@@ -1,4 +1,15 @@
 package com.example.metgalleryaf.data
 
+import com.example.metgalleryaf.model.Item
+
 class GalleryRepository {
+
+    val dummyItems = listOf<Item>(
+        Item(1),
+        Item(2),
+        Item(3),
+        Item(4),
+        Item(5)
+    )
+
 }
---
+++ app/src/main/java/com/example/metgalleryaf/model/Item.kt
@@ -0,0 +1,12 @@
+package com.example.metgalleryaf.model
+
+data class Item(
+    val objectID: Int,
+    /*val title: String,
+    val classification: String,
+    val artist: String,
+    val objectDate: String,
+    val primaryImg: String,
+    val smallImg: String,
+    val additionalImages: List<String>*/
+)
