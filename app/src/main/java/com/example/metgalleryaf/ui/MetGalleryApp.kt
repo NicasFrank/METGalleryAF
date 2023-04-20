c2c01134ef0fca5e10708155df8bfc1027982aa3
NicasFrank <nicas.frank@gmail.com>
2023-04-20 14:02:07 +0200

    extracted even more string resources

--- app/src/main/java/com/example/metgalleryaf/ui/MetGalleryApp.kt
+++ app/src/main/java/com/example/metgalleryaf/ui/MetGalleryApp.kt
@@ -7,8 +7,10 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.example.metgalleryaf.R
 import com.example.metgalleryaf.data.AppContainer
 import com.example.metgalleryaf.navigation.ItemDestination
 import com.example.metgalleryaf.navigation.MetGalleryNavHost
@@ -28,7 +30,7 @@ fun MetGalleryApp(appContainer: AppContainer) {
                     title = {
                         Text(
                             text = if (isItemScreen) currentBackStack?.arguments?.getInt(ItemDestination.itemIdArg).toString()
-                            else "Gallery"
+                            else stringResource(id = R.string.gallery_headline)
                         )
                     },
                     navigationIcon = {
@@ -36,7 +38,7 @@ fun MetGalleryApp(appContainer: AppContainer) {
                             IconButton(onClick = { navController.popBackStack() }, content = {
                                 Icon(
                                     imageVector = Icons.Default.ArrowBack,
-                                    contentDescription = "Return"
+                                    contentDescription = stringResource(id = R.string.navigate_back)
                                 )
                             })
                         }
