582b3ec454123aaf94981df5b51dd04404f3ed17
nicasfrank <nicas.frank@appsfactory.de>
2023-05-23 15:45:49 +0200

    Background

--- app/src/main/java/com/example/metgalleryaf/ui/MetGalleryWidget.kt
+++ app/src/main/java/com/example/metgalleryaf/ui/MetGalleryWidget.kt
@@ -1,11 +1,18 @@
 package com.example.metgalleryaf.ui
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.glance.GlanceModifier
+import androidx.glance.background
+import androidx.glance.layout.Box
+import androidx.glance.layout.fillMaxSize
 import androidx.glance.text.Text
 
 @Composable
 fun MetGalleryWidget() {
 
-    Text(text = "Peped")
+    Box(modifier = GlanceModifier.fillMaxSize().background(Color.Blue)) {
+        Text(text = "PPEPDEDE")
+    }
 
 }
